20abdceef9d0548e801d50b6a5f5ef358182735c
Kunal Gosar <gosarkunal9@gmail.com>
2016-05-29 00:33:30 -0500

    server auth done

--- server.js
+++ server.js
@@ -2,6 +2,11 @@
 var express = require('express');
 var app = express();
 
+// Connect to online mongodb Database
+var mongoose = require('mongoose');
+var connectionuri = require('./db_connect')();
+mongoose.connect(connectionuri);
+
 // Send index.html file to browser
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');

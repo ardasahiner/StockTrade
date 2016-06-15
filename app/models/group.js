b7c0feca34d0fde2c051bcd02e4d2218af52f372
Sahiner <asahiner@acr.org>
2016-06-15 13:22:26 -0400

    changed models, but not api (this may break things)

---
+++ app/models/group.js
@@ -0,0 +1,38 @@
+var mongoose  = require('mongoose');
+var Schema    = mongoose.Schema;
+var bcrypt    = require('bcrypt-nodejs');
+var ObjectId = Schema.Types.ObjectId;
+
+var GroupSchema = new Schema({
+  name: { type: String, required: true, index: { unique : true } },
+  group_admin: {type: ObjectId, required: true},
+  members: {type: [ObjectId], default: [group_admin]}, //idk if this works
+  creation_date: {type: Date, default: Date.now()},
+  isPrivate: Boolean, //option to be a private group (has a password)
+  password: String
+});
+
+GroupSchema.pre('save', function(next) {
+  var group = this;
+  if (!group.isModified('password')) return next();
+
+  bcrypt.hash(group.password, null, null, function(err, hash) {
+    group.password = hash;
+    next();
+  });
+});
+
+// Verify password is same as hashed version
+GroupSchema.methods.comparePassword = function(password) {
+  var group = this;
+
+  //ensure the group has a password to compare to (just in case frontend messes up)
+  if (this.isPrivate) {
+    return bcrypt.compareSync(password, group.password);
+  } else {
+    return true;
+  }
+};
+
+
+module.exports = mongoose.model('Group', GroupSchema);

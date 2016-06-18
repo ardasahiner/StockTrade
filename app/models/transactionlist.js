fb2db19d2e7e41c3aee6c81437d24859608c48d1
Arda Sahiner <sahinera@gmail.com>
2016-06-18 00:25:14 -0400

    not really sure

--- app/models/transactionlist.js
+++ app/models/transactionlist.js
@@ -1,13 +1,12 @@
-var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
+var mongoose  = require('mongoose');
+var Schema    = mongoose.Schema;
 var ObjectId = Schema.Types.ObjectId;
 var Transaction = require('./transaction')
 
-
 var TransactionListSchema = new Schema({
 
-    userId: {type: ObjectId, required: true},
-    transactionList: {type: [Transaction]}
+  userId: {type: ObjectId, required: true},
+  transactionList: {type: [Transaction]}
 });
 
 module.exports = mongoose.model('TransactionList', TransactionListSchema);

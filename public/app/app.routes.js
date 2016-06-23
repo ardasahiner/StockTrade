c96f582f91a6254a129a8880be4544b6bf6b950a
Kunal Gosar <kunal.gosar@berkeley.edu>
2016-06-23 10:43:26 -0500

    added some import files

--- public/app/app.routes.js
+++ public/app/app.routes.js
@@ -6,50 +6,50 @@ angular.module('app.routes', ['ui.router'])
 
   $stateProvider
 
-    .state('home', {
-      url: '/',
-      templateUrl: 'app/views/pages/unauthenticated/home.html'
-    })
+  .state('home', {
+    url: '/',
+    templateUrl: 'app/views/pages/unauthenticated/home.html'
+  })
 
-    .state('about', {
-      url: '/about',
-      templateUrl: 'app/views/pages/unauthenticated/aboutus.html',
-      controller: 'aboutController',
-      controllerAs: 'about'
-    })
+  .state('about', {
+    url: '/about',
+    templateUrl: 'app/views/pages/unauthenticated/aboutus.html',
+    controller: 'aboutController',
+    controllerAs: 'about'
+  })
 
-    .state('contact', {
-      url: '/contact',
-      templateUrl: 'app/views/pages/unauthenticated/contactus.html',
-      controller: 'contactController',
-      controllerAs: 'contact'
-    })
+  .state('contact', {
+    url: '/contact',
+    templateUrl: 'app/views/pages/unauthenticated/contactus.html',
+    controller: 'contactController',
+    controllerAs: 'contact'
+  })
 
-    .state('login', {
-      url: '/login',
-      templateUrl: 'app/views/pages/unauthenticated/login.html',
-      controller: 'loginController',
-      controllerAs: 'login'
-    })
+  .state('login', {
+    url: '/login',
+    templateUrl: 'app/views/pages/unauthenticated/login.html',
+    controller: 'loginController',
+    controllerAs: 'login'
+  })
 
-    .state('signup', {
-      url: '/signup',
-      templateUrl: 'app/views/pages/unauthenticated/signup.html',
-      controller: 'signupController',
-      controllerAs: 'signup'
-    })
+  .state('signup', {
+    url: '/signup',
+    templateUrl: 'app/views/pages/unauthenticated/signup.html',
+    controller: 'signupController',
+    controllerAs: 'signup'
+  })
 
-    .state('terms', {
-      url: '/terms',
-      templateUrl: 'app/views/pages/unauthenticated/termsofuse.html',
-      controller: 'termsofuseController',
-      controllerAs: 'termsofuse'
-    })
+  .state('terms', {
+    url: '/terms',
+    templateUrl: 'app/views/pages/unauthenticated/termsofuse.html',
+    controller: 'termsofuseController',
+    controllerAs: 'termsofuse'
+  })
 
-    .state('404', {
-      url: '/404',
-      templateUrl: 'app/views/pages/404.html',
-    });
+  .state('404', {
+    url: '/404',
+    templateUrl: 'app/views/pages/404.html',
+  });
 
 })
 

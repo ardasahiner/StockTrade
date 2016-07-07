a04fe03dac9d733f4c530a85656f204cb395685a
Kunal Gosar <kunal.gosar@berkeley.edu>
2016-07-07 14:48:14 -0500

    made buy/sell functions into modals to make room for the portfolio

--- public/app/controllers/stockController.js
+++ public/app/controllers/stockController.js
@@ -3,14 +3,19 @@ angular.module('stockController', [])
 .controller('stockController', function($rootScope, $location, Stocks) {
 
   var vm = this;
+  vm.processing = false;
 
   vm.buyStock = function() {
+
+    vm.processing = true;
 
     Stocks.buyStock(vm.buy.ticker, vm.buy.quantity)
     .then(function(data) {
       vm.buydata = data.data;
       vm.buydata.ticker = vm.buy.ticker;
       delete vm.buy;
+      vm.processing = false;
+      $("#buyModal").modal('hide');
       $("#confirmBuyModal").modal('show');
     });
 
@@ -18,24 +23,33 @@ angular.module('stockController', [])
 
   vm.confirmBuy = function() {
 
+    vm.processing = true;
+
     Stocks.confirmBuyStock(vm.buydata.ticker, vm.buydata.amount)
     .success(function(data) {
+      vm.processing = false;
       $("#confirmBuyModal").modal('hide');
     });
 
   };
 
   vm.cancelBuy = function() {
+    vm.processing = false;
+    $("#buyModal").modal('hide');
     $("#confirmBuyModal").modal('hide');
   };
 
   vm.sellStock = function() {
+
+    vm.processing = true;
 
     Stocks.sellStock(vm.sell.ticker, vm.sell.quantity)
     .then(function(data) {
       vm.selldata = data.data;
       vm.selldata.ticker = vm.sell.ticker;
       delete vm.sell;
+      vm.processing = false;
+      $("#sellModal").modal('hide');
       $("#confirmSellModal").modal('show');
     });
 
@@ -43,14 +57,19 @@ angular.module('stockController', [])
 
   vm.confirmSell = function() {
 
+    vm.processing = true;
+
     Stocks.confirmSellStock(vm.selldata.ticker, vm.selldata.quantity)
     .success(function(data) {
+      vm.processing = false;
       $("#confirmSellModal").modal('hide');
     });
 
   };
 
-  vm.cancelBuy = function() {
+  vm.cancelSell = function() {
+    vm.processing = false;
+    $("#sellModal").modal('hide');
     $("#confirmSellModal").modal('hide');
   };
 

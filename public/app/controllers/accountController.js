1dff2a7e92ace86e0fd8da30066f388889df614a
Arda Sahiner <sahinera@gmail.com>
2016-08-09 20:13:33 -0400

    caching pls

--- public/app/controllers/accountController.js
+++ public/app/controllers/accountController.js
@@ -1,6 +1,7 @@
 angular.module('accountController', ['ui.bootstrap'])
   .controller('accountController', function($location, User, Auth) {
     var vm = this;
+    vm.updateData = {};
     vm.loading = false;
 
     vm.getInfo = function() {
@@ -8,6 +9,9 @@ angular.module('accountController', ['ui.bootstrap'])
       Auth.getUser()
         .then(function(data) {
           vm.user = data.data;
+          console.log(vm.user);
+          vm.updateData.firstName = vm.user.firstName;
+          vm.updateData.lastName = vm.user.lastName;
           vm.loading = false;
         })
         .catch(function(err) {
@@ -27,45 +31,49 @@ angular.module('accountController', ['ui.bootstrap'])
       if (typeof vm.updateData == "undefined") {
         vm.error = "You must enter at least one field";
       } else {
-        if (typeof vm.updateData.firstName != "undefined") {
+        if (typeof vm.updateData.firstName != "undefined" && vm.updateData.firstName != vm.user.firstName) {
+          console.log(vm.user.firstName);
           userData["firstName"] = vm.updateData.firstName;
         }
-        if (typeof vm.updateData.lastName != "undefined") {
+        if (typeof vm.updateData.lastName != "undefined" && vm.updateData.lastName != vm.user.lastName) {
           userData["lastName"] = vm.updateData.lastName;
         }
-        if (typeof vm.updateData.botAccount != "undefined") {
+        if (typeof vm.updateData.botAccount != "undefined" && vm.updateData.botAccount != vm.user.botAccount) {
           userData["botAccount"] = vm.updateData.botAccount;
         }
         if (vm.updateData.password !=  vm.updateData.confirmPassword) {
           vm.error = "Passwords do not match!";
         } else {
-          if (typeof vm.updateData.password != "undefined" && vm.updateData.password.length < 6) {
+          if ((typeof vm.updateData.password != "undefined" && vm.updateData.password.length < 6) && vm.updateData.password != "") {
+              console.log(vm.updateData.password);
               vm.error = "Password must be at least 6 characters";
           } else {
-            if (typeof vm.updateData.password != "undefined") {
+            if (typeof vm.updateData.password != "undefined" && vm.updateData.password != '' ) {
 
               userData["password"] = vm.updateData.password;
             }
-            // request
-            console.log(userData);
-            // User.updateUser(vm.user.username, userData)
-            //   .success(function(data) {
-            //     vm.loading = false;
-            //     if (data.success) {
-            //       $location.path('/account');
-            //     } else {
-            //       vm.error = data.message;
-            //   });
+
+            if (Object.keys(userData).length != 0) {
+              // request
+              console.log(userData);
+              User.updateUser(vm.user.username, userData)
+                .success(function(data) {
+                  vm.loading = false;
+                  if (data.success) {
+                    console.log(data.success);
+                    $location.path('/account');
+                  } else {
+                    vm.error = data.message;
+                  }
+               });
+
+            } else {
+
+              vm.error = "You must enter at least one field";
+            }
           }
         }
       }
-
-
-      // if (vm.updateData.password != vm.updateData.confirmPassword) {
-      //   vm.error = "Passwords don't match!";
-      // } else if (vm.upateData.newPassword.length < 6) {
-      //   vm.error = "Password must be longer than 6 characters";
-      // }
-
+      vm.loading = false;
     };
   });
